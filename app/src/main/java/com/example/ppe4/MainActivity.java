781ed73d325b0c437eaeaee9fa2d69894b9b03bf
mevencan <mevencan@gmail.com>
2021-05-27 15:24:01 +0200

    Set-up application

---
+++ app/src/main/java/com/example/ppe4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ppe4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
